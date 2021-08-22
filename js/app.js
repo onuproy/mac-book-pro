7cec3e0a2c61837f99b81216e43309317a64112a
onuproy <anupme01@gmail.com>
2021-08-22 22:50:20 +0600

    html fix

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
 //memory code
-document.getElementById('8gbmemory').addEventListener('click',function(){
+document.getElementById('8gbmemory').addEventListener('click', function () {
     macbookmemory.innerText = '0';
     update()
 });
-document.getElementById('16gbmemory').addEventListener('click',function(){
+document.getElementById('16gbmemory').addEventListener('click', function () {
     macbookmemory.innerText = '180';
     update()
 });
 const macbookmemory = document.getElementById('mack-memory');
 
 //storage code
-document.getElementById('256gbstorage').addEventListener('click',function(){
+document.getElementById('256gbstorage').addEventListener('click', function () {
     macbookstorage.innerText = '0';
     update()
 });
-document.getElementById('512gbstorage').addEventListener('click',function(){
+document.getElementById('512gbstorage').addEventListener('click', function () {
     macbookstorage.innerText = '100';
     update()
 });
-document.getElementById('1tbstorage').addEventListener('click',function(){
+document.getElementById('1tbstorage').addEventListener('click', function () {
     macbookstorage.innerText = '180';
     update()
 });
 const macbookstorage = document.getElementById('mack-storage');
 
 //delivery code
-document.getElementById('freedelivery').addEventListener('click',function(){
+document.getElementById('freedelivery').addEventListener('click', function () {
     delivery.innerText = '0';
     update()
 });
-document.getElementById('expressdelivery').addEventListener('click',function(){
+document.getElementById('expressdelivery').addEventListener('click', function () {
     delivery.innerText = '20';
     update()
 });
 const delivery = document.getElementById('delivery-cost');
 
 //update total code
-const totalPrice = document.getElementById('total-price'); 
-const totalBottomPrice = document.getElementById('bottom-price'); 
-function update(){
+const totalPrice = document.getElementById('total-price');
+const totalBottomPrice = document.getElementById('bottom-price');
+function update() {
     const mackBookMemory = parseFloat(macbookmemory.innerText);
     const mackBookStorage = parseFloat(macbookstorage.innerText);
     const deliveryCost = parseFloat(delivery.innerText);
@@ -49,15 +49,21 @@ function update(){
 }
 
 //promo code
-document.getElementById('promo-button').addEventListener('click',function(){
-	const promoField = document.getElementById('promofield');
-	const promoInput = promoField.value;
-	if(promoInput == 'stevekaku'){
-		const discountPrice = parseInt((totalPrice.innerText)*80) / 100;
-		totalBottomPrice.innerText = discountPrice;
-	}
-	else{
-		alert("Invalid Promo Code");
-	}
-	promoField.value = '';
+document.getElementById('promo-button').addEventListener('click', function () {
+    const promoField = document.getElementById('promofield');
+    const promoInput = promoField.value;
+    const stevekaku = 20;
+    if (promoInput == 'stevekaku') {
+        const totalPrice2 = totalPrice.innerText;
+        const discountPrice = (totalPrice2 * stevekaku) / 100;
+        lessPrice = totalPrice2 - discountPrice;
+        totalBottomPrice.innerText = lessPrice;
+    }
+    else {
+        alert("Invalid Promo Code");
+    }
+    promoField.value = '';
 });
+
+
+// const discountPrice = parseInt(totalPrice.innerText) - (parseInt(totalPrice.innerText)*20)/100;
